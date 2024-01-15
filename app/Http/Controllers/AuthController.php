5276a54fe3dea09028d6f2ab149a2ac5aa5e3839
Tuntun pig <134057222+gogopinpin@users.noreply.github.com>
2024-01-15 11:40:09 +0800

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,10 +12,16 @@ class AuthController extends Controller
 {
     public function signup(CreatUser $request)
     {
+        
+        // 取得經過驗證的用戶輸入數據
         $validateData = $request->validated();
+
+        // 將經過驗證的數據填充到模型。
         $user = new User([
             'name' => $validateData['name'],
             'email' => $validateData['email'],
+
+            // 使用 bcrypt 函數對密碼進行加密。
             'password' => bcrypt($validateData['password']),
         ]);
         $user->save();
@@ -24,13 +30,19 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
+
+        // 驗證用戶輸入的電子郵件和密碼。
         $validateData = $request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string'
         ]);
+
+        // 嘗試進行用戶身份驗證。
         if (!Auth::attempt($validateData)){
             return response('授權失敗', 401);
         }
+
+        // 取得已經登入的用戶
         $user = $request->user();
         $tokenResult = $user->createToken('Token');
         $tokenResult->token->save();
